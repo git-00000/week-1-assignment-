c1d6fb90a63034c6488703122b3745ee1ddda23f
git-00000 <koushikbhowmick04@gmail.com>
2025-07-20 12:43:21 +0530

    first question

---
+++ assingment.c
@@ -0,0 +1,26 @@
+//  Largest Element in Array
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, max;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];  
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("The largest element is %d\n", max);
+    return 0;
+}
