b55136a6948902d43a3088a60c16bd8e99bb6ee7
git-00000 <koushikbhowmick04@gmail.com>
2025-07-20 13:14:31 +0530

    Update assingment.c

--- assingment.c
+++ assingment.c
@@ -24,3 +24,28 @@ int main() {
     printf("The largest element is %d\n", max);
     return 0;
 }
+
+#include <stdio.h>
+
+int findSecondLargest(int arr[], int n) {
+    int first = -1, second = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] < first) {
+            second = arr[i];
+        }
+    }
+
+    return second;
+}
+
+int main() {
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = findSecondLargest(arr, n);
+    printf("Second Largest: %d\n", result);
+    return 0;
+}
