0770b195c779de6684d0ea3938ba064cd3f60a3c
git-00000 <koushikbhowmick04@gmail.com>
2025-07-20 14:00:55 +0530

    Remove incorrect assingment file

--- assingment.py
+++
@@ -1,9 +0,0 @@
-def find_largest(arr):
-    max_element = arr[0]
-    for num in arr[1:]:
-        if num > max_element:
-            max_element = num
-    return max_element
-
-
-print("Largest:", find_largest([1, 8, 7, 56, 90])) 
